6a7ebde86b6a91437a1a29447863f2ba88d6a0ea
Nada <nsalem92@gmail.com>
2016-11-22 15:14:42 -0800

    Added Skeleton

--- R/sdnada.R
+++
@@ -1,7 +0,0 @@
-standard_deviation <- function(x) {
-	n <- length(x)
-	mean = sum(x) / n
-	ssq <- sum((x-mean)^2)
-	stddev = sqrt(ssq/n)
-	return(stddev)
-}
---
+++ R/standard_deviation.R
@@ -0,0 +1,19 @@
+#' Standard Deviation
+#'
+#' This contrived function computes the standard deviation of the values in x.
+#' @usage standard_deviation(x)
+#' @param x a numeric vector
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples Standard_deviation(c(1, 7, 3.7, 2, 0.3))
+
+standard_deviation <- function(x) {
+	n <- length(x)
+	mean = sum(x) / n
+	ssq <- sum((x-mean)^2)
+	stddev = sqrt(ssq/n)
+	return(stddev)
+}
+
